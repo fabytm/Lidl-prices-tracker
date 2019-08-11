4ce548484104fb0abc20e9a292a343303e224a02
Butean Fabian <buteanfabian@gmail.com>
2019-08-11 11:15:00 +0300

    Added folder creation if folder doesn't exist

--- PriceScraper.py
+++ PriceScraper.py
@@ -1,3 +1,4 @@
+import os
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import requests
@@ -12,18 +13,21 @@ current_timestamp = datetime.datetime.now()
 
 current_date = current_timestamp.strftime("%d-%m-%Y")
 
-def get_html(url): #gets html from url given
+
+def get_html(url):  # gets html from url given
     html = requests.get(url)
     content = html.content
     return BeautifulSoup(content, "html.parser")
+
+
 def get_column_range(nr_of_rows, nr_of_cols):
-    if(nr_of_cols <= 26):
+    if nr_of_cols <= 26 :
         return '{col_i}{row_i}:{col_f}{row_f}'.format(
             col_i = chr((nr_of_cols - 1) + ord('A')),  # converts number to letter
             col_f = chr((nr_of_cols - 1) + ord('A')),  # subtract 1 because of 0-indexing
             row_i = 1,
             row_f = nr_of_rows)
-    elif(nr_of_cols <= 676):
+    elif nr_of_cols <= 676 :
         return '{col_i}{col_ii}{row_i}:{col_f}{col_ff}{row_f}'.format(
             col_i = chr(int((nr_of_cols - 1) / 26 - 1) + ord('A')),  # converts number to letter
             col_ii = chr(int((nr_of_cols - 1) % 26) + ord('A')),
@@ -32,9 +36,8 @@ def get_column_range(nr_of_rows, nr_of_cols):
             row_i = 1,
             row_f = nr_of_rows)
 
-todays_url = "http://www.lidl.ro/ro/cataloage/preturi-valabile-astazi-" + current_date + "-preturi-valabile-astazi-" + current_date + "/view/menu/page/1"
 
-todays_url = "https://endpoints.lidl-flyer.com/v1/preturile-valabile-astazi-" +  current_date + "/flyer.json"
+todays_url = "https://endpoints.lidl-flyer.com/v1/preturile-valabile-astazi-" + current_date + "/flyer.json"
 
 print(todays_url)
 
@@ -48,17 +51,21 @@ print(pdf_url)
 header = urllib.request.Request(pdf_url, headers = {'User-Agent': 'Mozilla/5.0'})
 pdf = requests.get(pdf_url)
 
-with open('PDFs/currentPDF.pdf', "wb") as file:
+if not os.path.exists("PDF"):
+    os.mkdir("PDF")
+    print("Folder PDF created.")
+
+with open('PDF/currentPDF.pdf', "wb") as file:
     file.write(pdf.content)
 
-tables = camelot.read_pdf('PDFs/currentPDF.pdf', pages="all")
-#tables.export("test.csv", f='csv')
+tables = camelot.read_pdf('PDF/currentPDF.pdf', pages="all")
+# tables.export("test.csv", f='csv')
 sheet = spreadsheet.get_sheet('LidlPriceTracking.json','sheetID.txt')
 
 
 sheet_data = sheet.get_all_values()
-sheet_product_col = sheet.col_values(1)     #extract first column - product names
-sheet_quantity_col = sheet.col_values(2)    #extract second column - quantity - used for products that share the same name
+sheet_product_col = sheet.col_values(1)   # extract first column - product names
+sheet_quantity_col = sheet.col_values(2)  # extract second column - quantity - for products that share the same name
 sheet_date_row = sheet.row_values(1)
 
 rows_populated = len(sheet_product_col)
@@ -79,7 +86,7 @@ prices_list = sheet.range(get_column_range(rows_populated, cols_populated))
 
 for sublist in tables:
     for product in sublist.data:
-        if(product[0] != ''):
+        if product[0] != '':
             if sheet_product_col.count(product[0]) == 1:
                 prices_list[sheet_product_col.index(product[0])].value = product[3]
             elif sheet_product_col.count(product[0]) > 1:
@@ -98,7 +105,6 @@ for sublist in tables:
                 print(formatted_list)
                 to_append_list.append(formatted_list)
 print(prices_list)
-
 
 
 sheet.append_rows(to_append_list)
